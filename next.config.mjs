67cbc3a205f3bd6785dac18db0abcf0a870c9713
SHREYASTELKAR <shretel@gmail.com>
2024-02-25 20:47:32 -0800

    app method

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV = 'production';
+
 const nextConfig = {
   output: 'export',
-  
+  basePath: isProd ? 'http://shreyast.github.io':'',
   images: {
     unoptimized: true
   }
