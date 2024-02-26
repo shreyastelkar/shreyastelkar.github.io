0b097d65a7d114d61aca1bbdb7476617e7e9b578
SHREYASTELKAR <shretel@gmail.com>
2024-02-25 22:30:12 -0800

    Deploy 83033e1af9705e5ac7bfa6d7532cb5049a55d824

--- docs/_next/static/9b7mM5Il-yLVc8_ROxrdf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/ae2b08659ce351bc.css","static/chunks/pages/index-61f18e8da6b49651.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
