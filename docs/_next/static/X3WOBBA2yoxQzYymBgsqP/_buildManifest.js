01f5cb6664c2bdcbf5d6e6b4756a2ebb903a5120
SHREYASTELKAR <shretel@gmail.com>
2024-02-26 08:24:31 -0800

    Deploy 6bee90bf0ac7e129e23e91b8bb2a2a70b9f871f6

---
+++ docs/_next/static/X3WOBBA2yoxQzYymBgsqP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/700-6420e9f3de698b86.js","static/chunks/pages/index-095cb5af91fbabe1.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
