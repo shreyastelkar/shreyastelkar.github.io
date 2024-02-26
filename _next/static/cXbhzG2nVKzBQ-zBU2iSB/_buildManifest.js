065776a423a0657200abd7c7d6729e6996d30fb1
SHREYASTELKAR <shretel@gmail.com>
2024-02-26 09:39:31 -0800

    Updates

---
+++ _next/static/cXbhzG2nVKzBQ-zBU2iSB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/94726e6d-c4cbb5dcf369a3dc.js","static/chunks/673-9881d9b0386924c1.js","static/chunks/pages/index-e0ce8f53e6445d27.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
