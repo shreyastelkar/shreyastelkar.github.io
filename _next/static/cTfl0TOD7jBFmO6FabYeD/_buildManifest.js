98ad86bcfd80661f59eaeb85e6f208cf0ef61f16
SHREYASTELKAR <shretel@gmail.com>
2024-02-25 23:26:43 -0800

    Updates

--- _next/static/cTfl0TOD7jBFmO6FabYeD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/3cbd70ff48da2307.css","static/chunks/pages/index-4546e290d1e97ef7.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
