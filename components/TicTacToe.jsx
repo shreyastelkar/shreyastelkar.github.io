32c9e973541987af25f2c572cad6b1f109166c25
SHREYASTELKAR <shretel@gmail.com>
2024-04-16 18:07:09 -0700

    Working code - needs reformatting.

--- components/TicTacToe.jsx
+++ components/TicTacToe.jsx
@@ -2,23 +2,20 @@ import { useState } from "react";
 
 export const TicTacToe = () => {
   const style = {
-    width: 300,
-    height: 300,
+    width: 400,
+    height: 400,
     border: "1px solid",
     boxSizing: "content-box",
   };
-  const cols = 3;
-  const rows = 3;
+  const [cols, setCols] = useState(3);
+  const [rows, setRows] = useState(3);
   const pieces = ["X", "O"];
   const [turn, setTurn] = useState(0);
-  const [grid, setGrid] = useState(Array(9).fill(null));
+  const [grid, setGrid] = useState(Array(rows * cols).fill(null));
   const [win, setWin] = useState("in progress");
 
-  const notClicked = (i) => {
-    if (pieces.indexOf(grid[i]) < 0) {
-      return true;
-    }
-    return false;
+  const isEmpty = (i) => {
+    return grid[i] === null;
   };
 
   const gridIndex = (x, y) => {
@@ -40,15 +37,68 @@ export const TicTacToe = () => {
     return false;
   };
 
+  const checkWinVertical = (newGrid) => {
+    for (let x = 0; x < rows - 2; x += 1) {
+      for (let y = 0; y < cols; y += 1) {
+        if (
+          newGrid[gridIndex(x, y)] == newGrid[gridIndex(x + 1, y)] &&
+          newGrid[gridIndex(x + 1, y)] == newGrid[gridIndex(x + 2, y)] &&
+          newGrid[gridIndex(x, y)] != null
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+  const checkWinDiagonalRight = (newGrid) => {
+    for (let x = 0; x < rows - 2; x += 1) {
+      for (let y = 0; y < cols - 2; y += 1) {
+        if (
+          newGrid[gridIndex(x, y)] == newGrid[gridIndex(x + 1, y + 1)] &&
+          newGrid[gridIndex(x + 1, y + 1)] ==
+            newGrid[gridIndex(x + 2, y + 2)] &&
+          newGrid[gridIndex(x, y)] != null
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+  const checkWinDiagonalLeft = (newGrid) => {
+    for (let x = 0; x < rows - 2; x += 1) {
+      for (let y = cols - 1; y > 1; y -= 1) {
+        if (
+          newGrid[gridIndex(x, y)] == newGrid[gridIndex(x + 1, y - 1)] &&
+          newGrid[gridIndex(x + 1, y - 1)] ==
+            newGrid[gridIndex(x + 2, y - 2)] &&
+          newGrid[gridIndex(x, y)] != null
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+  const checkWin = (newGrid) => {
+    return (
+      checkWinDiagonalLeft(newGrid) ||
+      checkWinDiagonalRight(newGrid) ||
+      checkWinHorizontal(newGrid) ||
+      checkWinVertical(newGrid)
+    );
+  };
+
   const clickHandler = (i) => {
     const piece = pieces[turn];
     const newGrid = [...grid];
 
-    if (notClicked(i)) {
+    if (isEmpty(i) && win !== "You Win") {
       newGrid[i] = piece;
       setGrid(newGrid);
       setTurn((turn + 1) % pieces.length);
-      let isWin = checkWinHorizontal(newGrid);
+      let isWin = checkWin(newGrid);
 
       if (isWin == true) {
         setWin("You Win");
@@ -62,8 +112,45 @@ export const TicTacToe = () => {
 
   return (
     <>
-      <h1> {win} </h1>
-      <div style={style}>{gridJSX}</div>
+      <div> {win} </div>
+      <div
+        style={{
+          ...style,
+          width: `${cols * 100}px`,
+          height: `${rows * 100}px`,
+        }}
+      >
+        {gridJSX}
+      </div>
+
+      <div>
+        <label>
+          Number of Columns:
+          <input
+            value={cols}
+            onChange={(e) => {
+              setCols(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Number of Rows:
+          <input
+            value={rows}
+            onChange={(e) => {
+              setRows(e.target.value);
+            }}
+          />
+        </label>
+      </div>
+      <button
+        onClick={() => {
+          setGrid(Array(rows * cols).fill(null));
+          setWin("in progress");
+        }}
+      >
+        Reset
+      </button>
     </>
   );
 };
@@ -82,7 +169,7 @@ const Grid = ({ value, index, clickHandler }) => {
 
   return (
     <div style={style} onClick={handleClick}>
-      {value || "&nbsp;"}
+      {value || "-"}
     </div>
   );
 };
